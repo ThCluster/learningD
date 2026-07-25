fb29f7b6cbd5360a2f6f8079cca7672e67cb4ecd
DJABIA KASSI VINCENT ANDRE <vincentandredev@gmail.com>
2026-07-25 21:20:02 +0200

    Seconde Phase Learning of course

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "django_filters",
     "rest_framework.authtoken",
     "dj_rest_auth"
+    "corsheaders"
 ]
 
 
@@ -50,6 +51,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",  # <-- AJOUTEZ CETTE LIGNE
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -123,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+CORS_ALLOWED_ORIGINS = [
+    
+]
